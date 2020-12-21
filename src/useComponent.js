2c3e75017e804bdf0bd3ceddf4cb4ec6a151644a
qdl <levinqdl@magicengine.com.cn>
2020-12-21 18:43:18 +0800

    fix useComponent crash on emotion css prop

--- src/useComponent.js
+++ src/useComponent.js
@@ -1,13 +1,13 @@
-import React, { useRef } from "react"
+import React, { cloneElement, useRef } from "react"
 
 const useComponent = (children, formProps) => {
   const renderRef = useRef()
   renderRef.current = function Control(props) {
     if (typeof children === "function") {
       const control = children(formProps)
-      return <control.type {...control.props} {...props} />
+      return cloneElement(control, {...control.props, ...props})
     }
-    return <children.type {...formProps} {...children.props} {...props} />
+    return cloneElement(children, {...formProps, ...children.props, ...props})
   }
   const ref = useRef(function Component(props) {
     return renderRef.current(props)
