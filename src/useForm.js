252e62ee17d73c4f5635f404a07263879351ebf4
qdl <levinqdl@magicengine.com.cn>
2021-01-06 18:58:47 +0800

    keep useForm ref equal

--- src/useForm.js
+++ src/useForm.js
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { UPDATE } from "./events"
 import useStore from "./useStore"
 
 const useForm = (init) => {
   const updater = useState(0)[1]
-  const toSub = new Set()
+  const toSub = useRef(new Set())
   const store = useStore(init)
   useEffect(() => {
     const unsubs = []
-    for (const sub of toSub) {
+    for (const sub of toSub.current) {
       unsubs.push(
         store.observer.listen(UPDATE, () => updater((x) => x + 1), sub),
       )
@@ -19,20 +19,24 @@ const useForm = (init) => {
       }
     }
   })
-  return new Proxy(store, {
-    get(target, prop) {
-      if (prop === "get") {
-        return (path, noListen = false) => {
-          const s = store.get(path)
-          if (!noListen) {
-            toSub.add(path)
+  const formRef = useRef()
+  if (!formRef.current) {
+    formRef.current = new Proxy(store, {
+      get(target, prop) {
+        if (prop === "get") {
+          return (path, noListen = false) => {
+            const s = store.get(path)
+            if (!noListen) {
+              toSub.current.add(path)
+            }
+            return s
           }
-          return s
         }
-      }
-      return target[prop]
-    },
-  })
+        return target[prop]
+      },
+    })
+  }
+  return formRef.current
 }
 
 export default useForm
