e92f07041bb20660f08e2c62e1d9173fed4ac6bb
qdl <levinqdl@magicengine.com.cn>
2021-01-15 21:52:38 +0800

    impl fieldRef

--- spec/associate_update.spec.js
+++ spec/associate_update.spec.js
@@ -3,31 +3,39 @@ import { render, screen, waitFor } from "@testing-library/react"
 import App from "./App"
 import Field from "../src/Field"
 import userEvent from "@testing-library/user-event"
+import { useForm } from "../src/index"
 
 describe("associate_update", () => {
   test("basic", async () => {
-    render(
-      <App initValue={{ a: "", b: "" }}>
-        <Field
-          name="a"
-          label="A"
-          control={({ get, set }) => (
-            <input
-              value={get()}
-              onChange={({ target: { value } }) => {
-                set(value)
-                set(value, "b")
-              }}
-            />
-          )}
-        />
-        <Field name="b" label="B" />
-      </App>,
-    )
+    console.error = jest.fn()
+    const Container = () => {
+      const form = useForm({ a: "", b: "" })
+      const b = form.fieldRef("b")
+      return (
+        <App initValue={form}>
+          <Field
+            name="a"
+            label="A"
+            control={({ get, set }) => (
+              <input
+                value={get()}
+                onChange={({ target: { value } }) => {
+                  set(value)
+                  b.value = value
+                }}
+              />
+            )}
+          />
+          <Field name="b" label="B" />
+        </App>
+      )
+    }
+    render(<Container />)
     const inputA = screen.getByLabelText("A")
     userEvent.type(inputA, "123")
     await waitFor(() => {
       expect(screen.getByLabelText("B")).toHaveValue("123")
     })
+    expect(console.error).toHaveBeenCalledWith("b fieldRef already exists")
   })
 })
