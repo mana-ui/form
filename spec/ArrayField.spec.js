af9079435a1d37fbf39df48848534c84a9ae7b51
casanova <casanovaxie@magicengine.com.cn>
2021-01-26 18:50:15 +0800

    ✨  Listfied添加clear方法

--- spec/ArrayField.spec.js
+++ spec/ArrayField.spec.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import App from "./App"
-import { Field, ArrayField, useForm } from "../src"
+import { Field, useForm } from "../src"
 import { screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import useListField from "../src/useListField"
+import ListField from "../src/ListField"
 
 describe("IterableField", () => {
   test("list field", async () => {
@@ -16,12 +17,13 @@ describe("IterableField", () => {
       </span>
     )
     const List = ({ name }) => {
-      const [ListField, list] = useListField({ name })
+      const list = useListField({ field: name })
       return (
         <>
           <span onClick={() => list.append({ a: "a3" })}>Append</span>
           <span onClick={() => list.prepend({ a: "a4" })}>Prepend</span>
-          <ListField>
+          <span onClick={() => list.clear()}>Clear</span>
+          <ListField field={list}>
             <Item />
           </ListField>
         </>
@@ -53,33 +55,11 @@ describe("IterableField", () => {
     await waitFor(() => {
       expect(screen.getAllByLabelText("A")).toHaveLength(3)
     })
-    expect(console.error).not.toHaveBeenCalled()
-  })
-  test("fieldset with iterator", async () => {
-    const Container = () => {
-      const form = useForm([{ a: "a1" }, { a: "a2" }])
-      const root = form.field("")
-      return (
-        <App initValue={form}>
-          <div>array fields</div>
-          <span onClick={() => (root.value = [...root.value, { a: "a3" }])}>
-            Add
-          </span>
-          <ArrayField>
-            <Field name="a" label="A" />
-          </ArrayField>
-        </App>
-      )
-    }
-    render(<Container />)
-    expect(screen.getByText("array fields")).toBeInTheDocument()
-    const inputAs = screen.getAllByLabelText("A")
-    expect(inputAs).toHaveLength(2)
-    expect(inputAs[0]).toHaveValue("a1")
-    expect(inputAs[1]).toHaveValue("a2")
-    userEvent.click(screen.getByText("Add"))
+    userEvent.click(screen.getByText("Clear"))
     await waitFor(() => {
-      expect(screen.getAllByLabelText("A")).toHaveLength(3)
+      expect(screen.queryAllByLabelText("A")).toHaveLength(0)
     })
+
+    expect(console.error).not.toHaveBeenCalled()
   })
 })
